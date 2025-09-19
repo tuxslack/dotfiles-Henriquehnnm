7e266cce0752740d2129bc98d8060c2698797a6d
Henriquehnnm <henriquesardinha987@gmail.com>
2025-09-19 09:46:40 -0300

    Update

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,17 +1,36 @@
--- lua/plugins/conform.lua
-
 return {
   "stevearc/conform.nvim",
   opts = {
-    -- Aqui você define os formatadores para cada tipo de arquivo.
-    -- O LazyVim já cuida da formatação ao salvar.
+    -- formatadores por filetype
     formatters_by_ft = {
       css = { "prettier" },
       html = { "prettier" },
       javascript = { "prettier" },
       typescript = { "prettier" },
       json = { "prettier" },
-      -- Adicione aqui outros formatos que você quer que o Prettier cuide
+
+      -- Python: usa Black (e opcionalmente isort antes)
+      python = { "isort", "black" }, -- executa isort depois black
+    },
+
+    -- configurações adicionais (opcional)
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_fallback = true,
+    },
+
+    -- configurações dos formatadores
+    formatters = {
+      black = {
+        command = "black",
+        args = { "--quiet", "-" },
+        stdin = true,
+      },
+      isort = {
+        command = "isort",
+        args = { "--quiet", "-" },
+        stdin = true,
+      },
     },
   },
 }
