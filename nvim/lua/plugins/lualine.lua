df82f6b0abe0ac8aa5097c353d4b3fca72ef7a8b
Henriquehnnm <henriquesardinha987@gmail.com>
2025-09-19 08:16:48 -0300

    Update Vite status color on lualine config

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,33 +1,33 @@
 -- ~/.config/nvim/lua/plugins/lualine.lua
 return {
-  "nvim-lualine/lualine.nvim",
-  opts = function(_, opts)
-    -- Função para mostrar status do Vite Server
-    local function vite_server_status()
-      local ok, vs = pcall(require, "vite-server")
-      if not ok then
-        return ""
-      end
+	"nvim-lualine/lualine.nvim",
+	opts = function(_, opts)
+		-- Função para mostrar status do Vite Server
+		local function vite_server_status()
+			local ok, vs = pcall(require, "vite-server")
+			if not ok then
+				return ""
+			end
 
-      if vs.is_started then
-        local url = vs.gen_url(vs.config.vite_cli_opts)
-        return "󱐋 " .. url
-      else
-        return ""
-      end
-    end
+			if vs.is_started then
+				local url = vs.gen_url(vs.config.vite_cli_opts)
+				return "󱐋 " .. url
+			else
+				return ""
+			end
+		end
 
-    -- Adiciona o status do Vite na seção lualine_x
-    opts.sections = opts.sections or {}
-    opts.sections.lualine_x = opts.sections.lualine_x or {}
+		-- Adiciona o status do Vite na seção lualine_x
+		opts.sections = opts.sections or {}
+		opts.sections.lualine_x = opts.sections.lualine_x or {}
 
-    -- Insere o vite_server_status no início da seção lualine_x com cores
-    table.insert(opts.sections.lualine_x, 1, {
-      vite_server_status,
-      color = { fg = "#ffd866", gui = "bold" },
-      separator = { left = "", right = "" },
-    })
+		-- Insere o vite_server_status no início da seção lualine_x com cores
+		table.insert(opts.sections.lualine_x, 1, {
+			vite_server_status,
+			color = { fg = "#f6c177", gui = "bold" },
+			separator = { left = "", right = "" },
+		})
 
-    return opts
-  end,
+		return opts
+	end,
 }
